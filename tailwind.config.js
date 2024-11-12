7bda5d0203a0d3d7d0775de27d35dc9ba55672f0
jujudev7 <penna.julien@gmail.com>
2024-11-12 21:08:12 +0100

    fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./*.html", "./src/**/*.{js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anton: ["Anton", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        yellow: "#ffd15b",
+        grey: "#7A7A7A",
+        lightGrey: "#EDEDED",
+      },
+    },
   },
   plugins: [],
 };
-
